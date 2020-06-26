a4c00fc1c6568256ffabcb2ff37113b5fa7d546c
Echo2124 <echo2124@hotmail.com>
2020-06-26 18:20:57 +1000

    Began work on development

---
+++ main.js
@@ -0,0 +1,49 @@
+// AI Data Visualisation. Created by Josh S 2020.
+
+// Global Vars
+  var mini = true;
+
+function init() {
+  console.log("Initialising...");
+  sidebarManager();
+}; init();
+
+
+function toggleSidebar() {
+  if (mini) {
+    console.log("opening sidebar");
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    this.mini = false;
+  } else {
+    console.log("closing sidebar");
+    document.getElementById("mySidebar").style.width = "85px";
+    document.getElementById("main").style.marginLeft = "85px";
+    this.mini = true;
+  }
+}
+
+
+function generateChart(nodeName, data) {
+  var currentNode = document.createElement("canvas");
+  currentNode.classList.add("node-graph");
+  var ctx = document.getElementById(currentNode).getContext('2d');
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+}
